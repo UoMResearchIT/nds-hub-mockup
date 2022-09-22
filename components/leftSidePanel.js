038f5f0903b011cce7430f52661cad05823ee9f1
ChrisLam <fdq09eca@gmail.com>
2022-09-23 00:01:08 +0100

    add tabContents

--- components/leftSidePanel.js
+++ components/leftSidePanel.js
@@ -2,42 +2,65 @@ Vue.component('left-side-panel', {
     template:
         /* html */
         `
-    <template>
-  
     <v-navigation-drawer
       app
       permanent
       width="25%"
-    >
-    <template>
-    <v-tabs
+      >    
+      
+      <v-tabs
+      v-model="selectedTabIndex"
       fixed-tabs
-      background-color="indigo"
       dark
-    >
-      <v-tab v-for="tab in tabs">
-        <v-icon>{{tab.icon}}</v-icon>
-      </v-tab>
+      background-color="indigo"
+      class=" rounded-0"
+      width="100%"
+      >
+      <v-tab v-for="(tab, index) in tabs" :key="index" @click="Debug">
+    <v-icon>{{tab.icon}}</v-icon>
+    </v-tab>
     </v-tabs>
-  </template>
+    </div>
+    
+    
+    <v-container fill-height>
+
+    <p>current tab: {{this.currentTabName}}</p>
+
+    <tab-question-content v-if="this.currentTabName==='question'"></tab-question-content>
+    <tab-data-content v-if="this.currentTabName==='data'"></tab-data-content>
+    <tab-model-content v-if="this.currentTabName==='model'"></tab-model-content>
+    
+    </v-container>
     </v-navigation-drawer>
-  
-</template>
     `
     ,
 
     data() {
         return {
+            selectedTabIndex: 0,
             tabs: [
-                { title: 'Ask a Question', icon: 'mdi-head-question' },
-                { title: 'Do more with Data', icon: 'mdi-database-search' },
-                { title: 'Create a model', icon: 'mdi-chart-box' },
-            ],
-            links: ['Home', 'Contacts', 'Settings']
+                { name: 'question', title: 'Ask a Question', icon: 'mdi-head-question' },
+                { name: 'data', title: 'Do more with Data', icon: 'mdi-database-search' },
+                { name: 'model', title: 'Create a model', icon: 'mdi-chart-box' },
+            ]
         }
     },
 
     methods: {
-
+        Debug() {
+            /**/
+            console.log("selectedTabIndex: " + this.tabs[this.selectedTabIndex].name);
+        }
+    },
+    computed:{
+        currentTabName(){
+            return this.tabs[this.selectedTabIndex].name;
     }
-});
+}
+    
+
+
+
+    
+})
---
+++ components/tabDataContent.js
@@ -0,0 +1,41 @@
+Vue.component(
+    'tab-data-content', {
+        template:
+        /*html*/
+        `
+        <v-card
+        
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from DATA content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from DATA content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        Button
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+        `})
