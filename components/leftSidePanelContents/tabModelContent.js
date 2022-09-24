0517b822b112f6da9607b67562f16b806eef119a
gisvlasta <vasilis.vlastaras@gmail.com>
2022-09-24 20:41:46 +0100

    cleaned up a bit

--- components/leftSidePanelContents/tabModelContent.js
+++ components/leftSidePanelContents/tabModelContent.js
@@ -9,6 +9,228 @@ Vue.component(
     max-width="344"
     outlined
   >
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+  <v-row>
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">  
+        Hello from MODEL content!
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+        Hello from MODEL content!
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  
+  </v-row>
+
+
     <v-list-item three-line>
       <v-list-item-content>
         <div class="text-overline mb-4">  
