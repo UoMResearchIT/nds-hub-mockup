6a5d842a818419dc826e05c727a5d46f248da1e4
ChrisLam <fdq09eca@gmail.com>
2022-10-08 18:02:44 +0100

    init task-list

--- pinia-todo/src/App.vue
+++ pinia-todo/src/App.vue
@@ -3,19 +3,25 @@
     <header>
       <img src="./assets/logo.svg" alt="logo" />
       <h1>Pinia Todo</h1>
-      <h2>{{ taskStore.name }}</h2>
     </header>
+    <div class="task-list">
+        <div v-for="task in taskStore.tasks">
+            <Task :task="task" />
+        </div>
+    </div>
   </main>
 </template>
 
 <script>
 import { useTaskStore } from "./stores/taskStore";
-
+import Task from "./components/Task.vue";
 export default {
+  components: {
+    Task,
+  },
   setup() {
     const taskStore = useTaskStore();
     return { taskStore };
   },
 };
-
 </script>
